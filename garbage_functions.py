17b354886365e4406819710c6b0bad5f7f641e09
luciamartinf <luciamartinfernandez99@gmail.com>
2024-07-17 15:16:47 +0300

    Clean input file

--- garbage_functions.py
+++ garbage_functions.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import os
+import sys
+from Landmarks_module import Landmarks
 
 def check_file_options(file):
     """
@@ -43,3 +45,59 @@ def check_file_options(file):
             sys.exit()
     else:
         print(f"Proceeding with training. Generating {file} model. ")
+        
+
+
+def rewrite_file(txt_lmfile):
+    
+    """
+    Function to clean Landmarks input file
+    """
+    
+    with open(txt_lmfile, 'r') as file:
+        lines = file.readlines()
+    
+    new_file = 'Carabus_pronotumLM.txt'
+    
+    with open(new_file, 'w') as file:
+        
+        i = 0
+        while i < len(lines):
+            line = lines[i].strip()
+            
+            if line.startswith("LM"):
+                lm_list = []
+                n_lm = int(line.split('=')[1])
+                file.write(line+'\n')
+                
+                if n_lm == 0:
+                    print("WARNING: No landmarks annotated for this image")
+                    i += 1
+                    continue
+                
+                i += 1
+                for _ in range(n_lm):
+                    lm = list(map(float, lines[i].strip().split()))
+                    lm_list.append(lm)
+                    file.write(lines[i])
+                    i += 1
+                
+                if i < len(lines) and lines[i].startswith("IMAGE"):
+                    image_name = lines[i].strip().split('=')[1]
+                    file.write(lines[i])
+                    
+                    i += 1
+                    if i < len(lines) and lines[i].startswith("ID"):
+                        real_id = lines[i].strip().split('=')[1]
+                        img_id = Landmarks.check_id_img(real_id, image_name)
+                        file.write(f"ID={img_id}\n")
+                    
+                    i += 1
+                    if i < len(lines) and lines[i].startswith("SCALE"):
+                        file.write(lines[i])
+                    
+            i += 1
+
+if __name__ == "__main__":       
+    
+    rewrite_file('/Users/luciamf/Desktop/Landmarks_generator/Carabus_pronotumLANDMARKS.TXT')
