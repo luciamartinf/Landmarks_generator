a2dd22c973baec57cab883ee8c056b515f4fd67d
luciamartinf <luciamartinfernandez99@gmail.com>
2024-08-02 14:19:37 +0200

    change config annotations

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 # Number of threads/cores we'll be using when training our models. 
-PROCS = -1 # Si esta como -1 significa que vamos a usar todos los cores de la maquina
+PROCS = -1 
 
 # Maximum number of trials we'll be performing when tuning our shape predictor hyperparameters
-MAX_FUNC_CALLS = 50 # a lo mejor esto lo puedo tunear
+MAX_FUNC_CALLS = 50 
