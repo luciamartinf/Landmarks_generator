980df0fd737ffc80b4fbe07d74b5fc2786f85102
luciamartinf <luciamartinfernandez99@gmail.com>
2024-08-07 14:14:04 +0200

    reorganize_coor issue

--- reorganize_coor.py
+++ reorganize_coor.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.optimize import linear_sum_assignment
 
+
 def calculate_optimal_order(real_shape, measured_shape):
     
     """Calculate the optimal order of points in the measured shape to match the real shape."""
@@ -19,9 +20,24 @@ def reorganize_points(measured_shape, optimal_order):
     
     return measured_shape[optimal_order]
 
+def reorganize_all(real_shapes, measured_shapes):
+    
+    """Reorganize all points in a set of shapes"""
+    
+    optimal_order = calculate_optimal_order(real_shapes[0], measured_shapes[0])
+    
+    sorted_shapes = []
+    for shape in measured_shapes:
+        points = reorganize_points(shape, optimal_order)
+        sorted_shapes.append(points)
+        
+    return sorted_shapes
+
+  
 def main():
     
-    # reorganize points so they all have same order, esto tambien en el predict script
+    # reorganize points so they all have same order, esto tambien en el predict script dentro de predict_landmarks en Landmarks_module
+    # Pero para eso tendria que meter como argumento en ese script tambien el train.txt
 
     # transform xml in .tps and viceversa
     
