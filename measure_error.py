5a17a1cbd497617f27cfe045dca51809c3dcdb4b
luciamartinf <luciamartinfernandez99@gmail.com>
2024-08-23 13:37:29 +0200

    Edit descriptions

--- measure_error.py
+++ measure_error.py
@@ -22,29 +22,34 @@ import utils
 def main():
     
     parser = argparse.ArgumentParser(prog = '', formatter_class=argparse.RawDescriptionHelpFormatter,
-                                     description='Predict and measure the error of a model') # Elaborate this description
+                                     description='Predict test landmarks using the model and evaluate its performance by calculating the error.') 
     
     parser.add_argument('-f', '--file', required=True, 
-                        help = '.xml or .txt file that contains real landmarks')
+                        help = 'Reference _.tps_/_.xml_ file that contains landmarks')
     
     parser.add_argument('-i', '--image_dir', required = True,
                         help = 'Input directory containing the images')
     
     parser.add_argument('-m', '--model', required=True,
-                        help = ".dat file path of the LandmarkGen model.")
+                        help = ".dat file path of the model.")
     
     parser.add_argument('-o', '--output', 
-                        help = "Output file that will contain predicted landmarks in .tps")
+                        help = "Output .tps file that will contain predicted landmarks")
         
     args = parser.parse_args()
     
     xml = os.path.abspath(args.file)
     dat = os.path.abspath(args.model)
+    work_dir = os.path.dirname(dat)
+    
+    # Check that the model exist
+    dat = utils.check_predmodel(dat, work_dir, parser)
     
     Landmarks.data_dir = args.image_dir
     Landmarks.create_flipdir()
     
     set = Landmarks(xml) # img_list y lm_dict
+    
     mean_mre = set.calculate_error(dat)
 
     shapepred_fun.measure_mae(dat, xml)
