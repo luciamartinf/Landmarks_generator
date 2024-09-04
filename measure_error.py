1f37d8bcb91c1070006456af919243c70d3dea24
luciamartinf <luciamartinfernandez99@gmail.com>
2024-09-04 15:23:39 +0300

    Match scripts to readme

--- measure_error.py
+++ measure_error.py
@@ -16,7 +16,7 @@ import utils
 # Calculate mean relative error and other types, mean standard deviation ...
 
 
-# generate .tps prediction from xml_file =  output, only if output flag is on 
+
 
 
 def main():
@@ -24,28 +24,27 @@ def main():
     parser = argparse.ArgumentParser(prog = '', formatter_class=argparse.RawDescriptionHelpFormatter,
                                      description='Predict test landmarks using the model and evaluate its performance by calculating the error.') 
     
-    parser.add_argument('-f', '--file', required=True, 
+    parser.add_argument('-f', '--input_file', required=True, 
                         help = 'Reference _.tps_/_.xml_ file that contains landmarks')
     
-    parser.add_argument('-i', '--image_dir', required = True,
+    parser.add_argument('-i', '--input_dir', required = True,
                         help = 'Input directory containing the images')
     
     parser.add_argument('-m', '--model', required=True,
                         help = ".dat file path of the model.")
     
-    parser.add_argument('-o', '--output', 
-                        help = "Output .tps file that will contain predicted landmarks")
+    # TODO generate .tps prediction from xml_file =  output, only if output flag is on 
         
     args = parser.parse_args()
     
-    xml = os.path.abspath(args.file)
+    xml = os.path.abspath(args.input_file)
     dat = os.path.abspath(args.model)
     work_dir = os.path.dirname(dat)
     
     # Check that the model exist
     dat = utils.check_predmodel(dat, work_dir, parser)
     
-    Landmarks.data_dir = args.image_dir
+    Landmarks.data_dir = args.input_dir
     Landmarks.create_flipdir()
     
     set = Landmarks(xml) # img_list y lm_dict
