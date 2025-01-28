f4c50dbe1c364b44ea9d6a9060065316c5abfb18
Lucía Martín Fernández <luciamartinfernandez99@gmail.com>
2025-01-28 16:06:05 +0200

    make cross validation

--- cv.py
+++ cv.py
@@ -9,6 +9,7 @@ import multiprocessing
 import numpy as np
 import time
 import csv
+from sklearn.model_selection import KFold
 
 procs = multiprocessing.cpu_count()
 procs = config.PROCS if config.PROCS > 0 else (procs-1) 
@@ -19,53 +20,7 @@ def start_eval_file(eval_file):
         writer = csv.writer(file, delimiter='\t')
         writer.writerow(["cpu","Training_size", "Testing_size", "Tree_depth", "cascade_depth", "nu", "feature_pool_size", "num_test_splits", "oversampling_amount", "training_time", "training_error", "testing_error", "model_size"])
 
-def test_shape_predictor_params(
-        treeDepth, nu, cascadeDepth, featurePoolSize, numTestSplits, oversamplingAmount,
-        train_set, temp_dat):
-    
-    """Test parameters with a new split of the data
-    
-    Reference: 
-        https://pyimagesearch.com/2020/01/13/optimizing-dlib-shape-predictor-accuracy-with-find_min_global/
-    """
-    
-    # Create a new val-train split
-    train_xml, val_xml = train_set.split_data(tag = ['trn_val', 'validation'])
-    
 
-    # Define options that we are going to tune
-    options = dlib.shape_predictor_training_options()
-    options.tree_depth = int(treeDepth)
-    options.nu = nu
-    options.cascade_depth = int(cascadeDepth)
-    options.feature_pool_size = int(featurePoolSize)
-    options.num_test_splits = int(numTestSplits)
-    options.oversampling_amount = int(oversamplingAmount)
-
-	# tell dlib to be verbose when training and utilize our supplied number of threads when training
-    options.be_verbose = True	
-    options.num_threads = procs
-    
-    # display the current set of options to our terminal
-    print("[INFO] starting training with another split...")
-    print(options)
-    sys.stdout.flush()
-
-	# train the model using the current set of hyperparameters
-    dlib.train_shape_predictor(train_xml, temp_dat, options)
-
-	# take the newly trained shape predictor model and evaluate it on both our training and validating sets
-    trainingError = dlib.test_shape_predictor(train_xml, temp_dat)
-    testingError = dlib.test_shape_predictor(val_xml, temp_dat)
-    
-	# display the training and validation errors for the current trial
-    print("[INFO] Train error (MSE): {}".format(trainingError))
-    print("[INFO] Validation error (MSE): {}".format(testingError))
-    sys.stdout.flush()
-    
-	# return the error on the testing set
-    return testingError    
-    
 def eval_model(
         treeDepth, nu, cascadeDepth, featurePoolSize, numTestSplits, oversamplingAmount,
         train_set, temp_dat):
@@ -76,59 +31,67 @@ def eval_model(
         https://pyimagesearch.com/2020/01/13/optimizing-dlib-shape-predictor-accuracy-with-find_min_global/
     """
     
-    eval_file = 'eval_3.tsv'
-    split_per = [0.6, 0.4]
-    
-    # Create a new val-train split
-    train_xml, val_xml = train_set.split_data(tag = ['trn_val', 'validation'], split_size=split_per)
-    
-    
+    eval_file = 'Evaluation.tsv'
+    # split_per = [0.6, 0.4]
+    kf = KFold(n_splits=5)
     total_size = len(train_set.img_list)
-    training_size = round(split_per[0]*total_size)
-    testing_size = round(split_per[1]*total_size)
-
-    # Define options that we are going to tune
-    options = dlib.shape_predictor_training_options()
-    options.tree_depth = int(treeDepth)
-    options.nu = nu
-    options.cascade_depth = int(cascadeDepth)
-    options.feature_pool_size = int(featurePoolSize)
-    options.num_test_splits = int(numTestSplits)
-    options.oversampling_amount = int(oversamplingAmount)
-
-	# tell dlib to be verbose when training and utilize our supplied number of threads when training
-    options.be_verbose = True	
-    options.num_threads = procs
     
-    # display the current set of options to our terminal
-    print("[INFO] starting training with another split...")
-    print(options)
-    sys.stdout.flush()
-
-    start_time = time.time()
-	# train the model using the current set of hyperparameters
-    dlib.train_shape_predictor(train_xml, temp_dat, options)
-    end_time = time.time()
-    training_time = end_time - start_time
+    training_error = []
+    validating_error = []
+    for i, (train_index, test_index) in enumerate(kf.split(train_set)):
+        print(f"Fold {i}:")
+        
+        training_size = len(train_index)
+        testing_size = len(test_index)
     
-    model_size = os.path.getsize(temp_dat) / 1024
+        # Create a new val-train split
     
+        train_xml, val_xml = train_set.fold_data(train_index, test_index, tag = ['trn_val', 'validation'])
+    
+        # Define options that we are going to tune
+        options = dlib.shape_predictor_training_options()
+        options.tree_depth = int(treeDepth)
+        options.nu = nu
+        options.cascade_depth = int(cascadeDepth)
+        options.feature_pool_size = int(featurePoolSize)
+        options.num_test_splits = int(numTestSplits)
+        options.oversampling_amount = int(oversamplingAmount)
 
-	# take the newly trained shape predictor model and evaluate it on both our training and validating sets
-    trainingError = dlib.test_shape_predictor(train_xml, temp_dat)
-    testingError = dlib.test_shape_predictor(val_xml, temp_dat)
+        # tell dlib to be verbose when training and utilize our supplied number of threads when training
+        options.be_verbose = True	
+        options.num_threads = procs
+        
+        # display the current set of options to our terminal
+        print("[INFO] starting training with another split...")
+        print(options)
+        sys.stdout.flush()
+
+        start_time = time.time()
+        # train the model using the current set of hyperparameters
+        dlib.train_shape_predictor(train_xml, temp_dat, options)
+        end_time = time.time()
+        training_time = end_time - start_time
+        
+        model_size = os.path.getsize(temp_dat) / 1024
+        
+        # take the newly trained shape predictor model and evaluate it on both our training and validating sets
+      
+        training_error.append(dlib.test_shape_predictor(train_xml, temp_dat))
+        validating_error.append(dlib.test_shape_predictor(val_xml, temp_dat))
+        trainingError = np.mean(training_error)
+        validatingError = np.mean(validating_error)
     
 	# display the training and validation errors for the current trial
-    print("[INFO] Train error (MSE): {}".format(trainingError))
-    print("[INFO] Validation error (MSE): {}".format(testingError))
+    print("[INFO] Train error (MSE): {}".format(np.mean(trainingError)))
+    print("[INFO] Validation error (MSE): {}".format(np.mean(validatingError)))
     sys.stdout.flush()
     
     with open(eval_file, 'a', newline='') as file:
         writer = csv.writer(file, delimiter='\t')
-        writer.writerow([procs, training_size, testing_size, treeDepth, cascadeDepth, nu, featurePoolSize, numTestSplits, oversamplingAmount, training_time, trainingError, testingError, model_size])
+        writer.writerow([procs, training_size, testing_size, treeDepth, cascadeDepth, nu, featurePoolSize, numTestSplits, oversamplingAmount, training_time, trainingError, validatingError, model_size])
     
 	# return the error on the testing set
-    return testingError
+    return validatingError
 
 
 
@@ -162,13 +125,13 @@ def find_best_params(
         bound1=lower,
         bound2=upper,
         is_integer_variable=isint,
-        num_function_calls=config.MAX_FUNC_CALLS,
-        solver_epsilon=37.0
+        num_function_calls=config.MAX_FUNC_CALLS
+        # solver_epsilon=35.0
         )
     
     # display the optimal hyperparameters so we can reuse them in our training script
     print("[INFO] optimal parameters: {}".format(bestParams))
-    print("[INFO] optimal error: {}".format(bestLoss)) # bestLoss is just lower testing_error
+    print("[INFO] optimal error: {}".format(bestLoss)) # bestLoss is just lower ValidatingError
 
     # delete the temporary model file
     os.remove(temp_dat)
