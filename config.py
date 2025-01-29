f814788d156726474b91d88e933f727d4b02680b
luciamartinf <luciamartinfernandez99@gmail.com>
2025-01-29 14:10:15 +0200

    prepare for benchmark

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 # Number of threads/cores we'll be using when training our models. 
-PROCS = -1 
+# PROCS = -1 
+PROCS =  40
 
 # Maximum number of trials we'll be performing when tuning our shape predictor hyperparameters
 MAX_FUNC_CALLS = 20
